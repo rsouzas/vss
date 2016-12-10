1d9b38f26e3deaca37bc1ba260923e2a30e4aeea
Rafael Santos <rsouzasnts@gmail.com>
2016-12-10 11:31:52 -0200

    Adição de previsão (#1)
    * Adição de previsão
    
    - Modularização do Simulador.cpp e import do Simulador.h
    - Salva as variaveis de estado.
    - Estratégia chama o Simulador a cada ciclo do atacante.
    - Simulador execulta 30 vezes e caso a bola esteja perto do atacante (4cm), muda o objetivo pro estado do atacante simulado.
    - Retorna as variaveis de estado.

--- Futebol.cpp
+++ Futebol.cpp
@@ -45,6 +45,7 @@ bool emPenalidade = false;
 bool emPosiciona = false;
 bool emInicio = false;
 bool tiroMeta = false;
+bool emSimulacao = false;
 
 Objetivo objetivoRobo[NUM_ROBOS_TIME];
 bool chuteGirando[NUM_ROBOS_TIME];
